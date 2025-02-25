f0f60efa5bb814ed4f8da131c06f21598a9bb087
joelhsaltz <joelhsaltz@gmail.com>
2025-02-25 10:42:46 -0500

    changed 10 to 12

--- factorial.py
+++ factorial.py
@@ -1,8 +1,8 @@
 
 import math
 
-# Compute factorials of numbers from 1 to 10
-factorials = {num: math.factorial(num) for num in range(1, 11)}
+# Compute factorials of numbers from 1 to 12
+factorials = {num: math.factorial(num) for num in range(1, 13)}
 
 # Print the computed factorials
 for num, fact in factorials.items():
