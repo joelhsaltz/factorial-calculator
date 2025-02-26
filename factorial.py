4b9da1f0cd78e793ec0ef3f824e7eed8f4aa2f73
joelhsaltz <joelhsaltz@gmail.com>
2025-02-26 07:13:45 -0500

    added squares

--- factorial.py
+++ factorial.py
@@ -7,3 +7,11 @@ factorials = {num: math.factorial(num) for num in range(1, 13)}
 # Print the computed factorials
 for num, fact in factorials.items():
     print(f"Factorial of {num} is {fact}")
+
+# Compute squares  of numbers from 1 to 12
+squares = {num: num**2 for num in range(1, 13)}
+
+
+# Print the computed squaresf
+for num, square  in squares.items():
+    print(f"square of {num} is {square}")
